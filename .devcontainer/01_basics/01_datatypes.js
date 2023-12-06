7176b4ba431c68325a550b5979d4d71725e55045
g0kuluppal08 <97520505+g0kuluppal08@users.noreply.github.com>
2023-12-06 22:35:47 +0000

    basics

---
+++ .devcontainer/01_basics/01_datatypes.js
@@ -0,0 +1,9 @@
+"use strict" //treat new version of js
+
+//null
+//symbol
+//undefined
+let age="eighteen"
+
+console.log( typeof age)
+console.log(typeof null) // null is  of type object
---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const a=123
+let id="g@gmail.com"
+ids="amritsar"
+let g
+
+console.table([a, id, ids,g])
+
